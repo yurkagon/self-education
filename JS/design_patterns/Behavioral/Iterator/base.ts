e6894e18b8e5b79d76fa575b920a9182e68fac70
yurkagon <owngameplay@gmail.com>
2019-07-08 18:24:10 +0300

    [ADD] iterator design pattern

---
+++ JS/design_patterns/Behavioral/Iterator/base.ts
@@ -0,0 +1,31 @@
+class ArrayIterator {
+  private index: number = 0;
+  private items: Array<any>;
+
+  constructor(items: Array<any>) {
+    this.items = items;
+  }
+
+  public reset() {
+    this.index = 0;
+  }
+
+  public first() {
+    this.reset();
+    return this.next();
+  }
+
+  public next() {
+    return this.items[this.index++];
+  }
+
+  public hasNext() {
+    return this.index <= this.items.length;
+  }
+
+  public each(callback) {
+    for (var item = this.first(); this.hasNext(); item = this.next()) {
+      callback(item);
+    }
+  }
+}
